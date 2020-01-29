6d71887ff9d39dec4c153bf559ff613f45abeda1
Erick <erickescribaa@gmail.com>
2020-01-29 15:46:03 -0500

    (#1) chore: add Router and Apollo dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,24 @@
 import React from "react";
 import ApolloClient from "apollo-boost";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { ApolloProvider } from "react-apollo";
-import "./App.css";
+import Launches from "./Components/Launches";
+import Launch from "./Components/Launch";
 
 const client = new ApolloClient({
-  uri: "http://localhost:5000/graphql"
+  uri: "/graphql"
 });
 
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className="mt-5">
-        <h1 className="text-center">SpaceX</h1>
-      </div>
+      <Router>
+        <div className="container">
+          <h1 className="display-3 my-4 text-center">SPACEX</h1>
+          <Route exact path="/" component={Launches} />
+          <Route exact path="/launch/:flight_number" component={Launch} />
+        </div>
+      </Router>
     </ApolloProvider>
   );
 }
