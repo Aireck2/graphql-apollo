66391f05609bfcc18ef7755f0a6bf6e221895b57
Erick <erickescribaa@gmail.com>
2020-01-29 15:52:53 -0500

    (#1) feat: create index as MissionKey Component

---
+++ client/src/Components/MissionKey.js
@@ -0,0 +1,12 @@
+import React from "react";
+const MissionKey = () => (
+  <div className="my-3 " style={{ color: "white", fontWeight: "bold" }}>
+    <p>
+      <span className="p-2 mr-2 bg-success">= Success</span>
+    </p>
+    <p>
+      <span className="p-2 mr-2 bg-danger">= Fail</span>
+    </p>
+  </div>
+);
+export default MissionKey;
